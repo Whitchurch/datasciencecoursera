ab5594ab2dcd37917dab4348e9b897c7706441b5
Whitchurch Muthumani <neoblackcyptron@gmail.com>
2020-03-18 16:35:42 -0700

    push in the changes

--- DukeFolder/Course2Programming/workplacebiasinference.R
+++ DukeFolder/Course2Programming/workplacebiasinference.R
@@ -20,6 +20,23 @@ str(difference)
 ggplot(difference,aes(x = Male))+geom_histogram()
 summary(difference)
 difference%>%summarise(meanvalue = mean(Male))
+SE <- sqrt((0.05167*(1-0.05167))/35)
 
-#It looks like the null hypothesis is true , and the difference is purely by chance.
+#Calculate a 95# confidence interval to see what er get
+
+UB <- 0.05167 + 1.96*(SE)
+LB <- 0.05167 - 1.96*(SE)
+
+UB*100
+LB*100
+
+# We are 95% confident that a difference of 30% between male and female hiring does not exist in the population by random chance.
+# We can reject the null hypothesis.
+
+# To clarift further we calculate the Z-score to calculate the p-value
+Zscore <- 0.3-0.05167/SE
+pnorm(-1.080936,0.05167,SE,TRUE)
+
+# We get an impossibly low p-value, so we reject the null-hypothesis strongly and can accept the alternative.
+
 
