3d1f24508c1ab521a832b55a0c919fe4461ad84f
Whitchurch Muthumani <neoblackcyptron@gmail.com>
2020-03-21 22:33:58 -0700

    commit

--- DukeFolder/Course2Programming/AnswesQuiz3.R
+++ DukeFolder/Course2Programming/AnswesQuiz3.R
@@ -59,3 +59,28 @@ inference(y = gained,  data = nc, statistic = "mean", type = "ci",
           alternative = "twosided", method = "theoretical")
 
 #The average weight gained by young mothers and mature mothers is equal, there is no noticable difference
+
+#Run a hypothesis test to see if there is any difference between mean weights of babies born to smokers versus non-smoker moms
+inference(y = weight, x = habit, data = nc, statistic = "mean", type = "ht", null = 0, 
+          alternative = "twosided", method = "theoretical")
+
+#Calculate 95% confidence interval
+inference(y = weight, x = habit, data = nc, statistic = "mean", type = "ci", null = 0, 
+          alternative = "twosided", method = "theoretical")
+
+#Run hypothesis on whether being white vesus nonwhite has any influence on motherws age
+inference(y = mage, x = whitemom, data = nc, statistic = "mean", type = "ht", null = 0, 
+          alternative = "twosided", method = "theoretical")
+
+#Calculate onfidence interval
+inference(y = mage, x = whitemom, data = nc, statistic = "mean", type = "ci", null = 0, 
+          alternative = "twosided", method = "theoretical")
+
+#See if there is difference in mean age between white and non-white mons
+nc%>%select(mage,whitemom)%>%group_by(whitemom)%>%summarise(mean(mage), mean(mage))
+
+#plot a box plot to be doubly sure
+ggplot(data = nc, aes(x = whitemom, y = mage ))+geom_boxplot()
+
+#Conclusion: There is overwhelimg evidence that race(white vs non-white) plays a huge role, so there is a difference between 
+# mean ages of white and non-white ewomen , non-white women get pregnant earlier.
